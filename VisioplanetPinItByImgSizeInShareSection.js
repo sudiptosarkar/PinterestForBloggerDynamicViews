f295f885a9401dabcb093e90c7b59278c2667adf
sudiptosarkar <sudiptosarkar@visioplanet.org>
2013-10-12 18:57:26 +0530

    Update VisioplanetPinItByImgSizeInShareSection.js
    Added logging for all image urls for debugging.
    Code change for issue reported by Tony Valentino: https://plus.google.com/102518617013845257443
    
    Image in the post doesn't get detected in $("img") in any browser other than firefox. (Tested chrome, safari, IE and firefox).
    
    It works for http://www.visiolanet.org just fine, even in the same template.
    
    Page issue was reported on: http://blog.xdrproducts.com/2013/07/10-things-to-consider-when-buying.html
    
    Template issue was detected on: sidebar

--- VisioplanetPinItByImgSizeInShareSection.js
+++ VisioplanetPinItByImgSizeInShareSection.js
@@ -48,7 +48,7 @@ function visioplanetPinItInit(){
             var theWidth = $(this).width();
             var theHeight = $(this).height();
 
-            LOG && console.debug("Another loop iteration.");
+            LOG && console.debug("Parsing: " + imgURL);
             if((theWidth >= 400) || (theHeight >= 400)) {
               imgURL = $(this).attr("src");
               LOG && console.debug("Got the image URL: " + imgURL);
