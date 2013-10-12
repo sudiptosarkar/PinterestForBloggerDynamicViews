e626d73e2f83eda56aac107d8438cda705aa80ad
sudiptosarkar <sudiptosarkar@visioplanet.org>
2013-10-12 22:28:32 +0530

    Update VisioplanetPinItByImgSizeInShareSection.js
    Added timestamps. Now the duplication can be looked into.

--- VisioplanetPinItByImgSizeInShareSection.js
+++ VisioplanetPinItByImgSizeInShareSection.js
@@ -7,15 +7,15 @@ var pinitInitInitialized = false;
   try {
     // In case of insite navigation, document.ready will work.
     $(document).ready(function(){
-      LOG && console.debug("DOM is ready. Time to start up Pin It.");
+      LOG && console.debug(getTime + "    " + "DOM is ready. Time to start up Pin It.");
       visioplanetPinItInit();
     });
     $(window).load(function() {
-      LOG && console.debug("Window load is complete. Time to start up Pin It.");
+      LOG && console.debug(getTime + "    " + "Window load is complete. Time to start up Pin It.");
       visioplanetPinItInit();
     });
   } catch(e) {
-    console.error('Visioplanet Pinit Initialization Exception: ' + e);
+    console.error(getTime + "    " + 'Visioplanet Pinit Initialization Exception: ' + e);
     windowLoadMethod();
   }
 })();
@@ -24,30 +24,31 @@ function windowLoadMethod() {
   try {
     // If this is a direct post load, document.ready may not work and we will have to use window.load
     $(window).load(function() {
+      console.debug(getTime + "    " + "Initializing from failsafe.")
       visioplanetPinItInit();
     });
   } catch(e) {
-    console.error('Visioplanet Pinit Initialization Exception: ' + e);
+    console.error(getTime + "    " + 'Visioplanet Pinit Initialization Exception: ' + e);
   }
 }
 
 function visioplanetPinItInit(){
   setTimeout (
     function() {
-      LOG && console.debug("document status: " + document.readyState);
+      LOG && console.debug(getTime + "    " + "document status: " + document.readyState);
       if (document.readyState !== 'complete') {
-        LOG && console.debug("Document's not ready. Waiting...");
+        LOG && console.debug(getTime + "    " + "Document's not ready. Waiting...");
         //setTimeout(visioplanetPinItInit,1000);
       } else {
         // The following line will make sure that PinIt button initialization runs only once.
         // So that multiple PinIt buttons don't show up.
         if(pinitInitInitialized == true) {
-          LOG && console.debug("PinIt initialization already started. Exiting visioplanetPinItInit() to avoid duplicates.");
+          LOG && console.debug(getTime + "    " + "PinIt initialization already started. Exiting visioplanetPinItInit() to avoid duplicates.");
           return;
         }
         
         // We're gonna wait for sometime and then do our work (URL remains the old one otherwise).
-	    LOG && console.debug("Let's begin PinIt Initialization...");
+	    LOG && console.debug(getTime + "    " + "Let's begin PinIt Initialization...");
         // We're gonna put the button on top of the image at the top right corner of it.
         // For that, we'd have to check the width of the image.
 
@@ -63,18 +64,18 @@ function visioplanetPinItInit(){
             img.onload = function() {
                 theWidth = this.width;
                 theHeight = this.height;
-                LOG && console.debug(this.width + "x" + this.height);
+                LOG && console.debug(getTime + "    " + this.width + "x" + this.height);
             };
             
             theWidth = img.width;
             theHeight = img.height;
 
-            LOG && console.debug($(this).attr("src") + ": " + theWidth + "x" + theHeight);
+            LOG && console.debug(getTime + "    " + $(this).attr("src") + ": " + theWidth + "x" + theHeight);
             if((theWidth >= 400) || (theHeight >= 400)) {
               imgURL = $(this).attr("src");
-              LOG && console.debug("Got the image URL: " + imgURL);
+              LOG && console.debug(getTime + "    " + "Got the image URL: " + imgURL);
               return false;
-              LOG && console.debug("This line shouldn't come up.");
+              LOG && console.debug(getTime + "    " + "This line shouldn't come up.");
             }
         });
 
@@ -84,7 +85,7 @@ function visioplanetPinItInit(){
         htmlStr += imgURL;
         htmlStr += "'><img border='0' src='http://assets.pinterest.com/images/PinExt.png' title='Pin It'/></a>";
 
-        LOG && console.debug("The tag's as follows:\n\n" + htmlStr);
+        LOG && console.debug(getTime + "    " + "The tag's as follows:\n\n" + htmlStr);
 
         $(htmlStr).insertAfter("span.share-facebook.delay");
         // We're placing this button right after the +1 button.
